ba17792c836319140af966dfad34efc0d2e20d55
Kat Gonzalez <86013814+kathe92@users.noreply.github.com>
2022-06-20 20:00:37 -0600

    Add files via upload

---
+++ WEEK-11/Exercise 1 - Github/controller.js
@@ -0,0 +1,9 @@
+'use strict';
+
+exports.pong = function(req, res) {
+    res.json('pong');
+};
+
+exports.ping = function(req, res) {
+    res.json('ping');
+};
---
+++ WEEK-11/Exercise 1 - Github/routes.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = function(app) {
+    var controller = require('./controller');
+
+    app.route('/ping')
+        .get(controller.pong)
+
+    app.route('/pong')
+        .get(controller.ping)
+
+    app.route('/')
+        .get(controller.pong)
+};
